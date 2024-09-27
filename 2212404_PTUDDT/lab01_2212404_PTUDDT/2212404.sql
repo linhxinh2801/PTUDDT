68cdb9e48fac67b1142d5593ef22791c61eb6a1e
linhxinh2801 <2212404@dlu.edu.vn>
2024-09-27 16:13:25 +0700

    Add files via upload

---
+++ 2212404_PTUDDT/lab01_2212404_PTUDDT/2212404.sql
@@ -0,0 +1,87 @@
+﻿CREATE PROCEDURE Category_GetAll
+AS
+BEGIN
+	SELECT FROM dbo. Category
+END
+GO
+CREATE PROCEDURE Category_GetAll
+AS
+BEGIN
+	SELECT FROM dbo. Category
+END
+GO
+CREATE PROCEDURE Category_GetAll
+( 
+@ID INT
+)
+AS
+BEGIN
+	SELECT FROM dbo. Category WHERE ID = @ID
+END
+GO
+CREATE PROCEDURE Category_Insert
+(
+@Name NVARCHAR(1000),
+@Type INT
+)
+AS
+BEGIN
+ IF (NOT EXISTS (SELECT Name FROM dbo. Category WHERE Name = @Name 
+ INSERT INTO dbo. Category (Name, Type) 
+	VALUES (@Name, @Type)
+END 
+GO
+CREATE PROCEDURE Category_Update
+(
+@ID INT,
+@Name NVARCHAR(1000),
+@Type INT
+)
+AS
+BEGIN
+	UPDATE dbo.Category
+	SET [Name] =@Name, [Type] =@Type
+	WHERE ID = @ID
+END
+GO
+CREATE PROCEDURE Category_Delete
+(
+@ID INT
+)
+AS
+BEGIN
+	DELETE FROM dbo. Category
+	WHERE ID = @ID
+END
+GO
+EXEC dbo.Category_Insert N'Tráng miệng', 1
+ALTER PROCEDURE dbo. Category_Insert_
+(
+@ID INT OUTPUT,
+@Name NVARCHAR(1000),
+@Type INT
+)
+AS
+BEGIN
+
+	IF (NOT EXISTS (SELECT Name FROM dbo. Category WHERE Name = @Name))
+		INSERT INTO dbo. Category (Name, Type)
+			VALUES (@Name, @Type)
+	SET @ID = @@IDENTITY 
+END
+DECLARE @ID INT = 0;
+EXEC dbo.Category_Insert_ @ID = @ID OUTPUT, 
+						  @Name = N'Món rau', 
+						  @Type = 1
+SELECT*FROM dbo. Category WHERE ID = @ID
+CREATE PROCEDURE [dbo].[GetAll]
+(
+@TableName NVARCHAR(200)
+)
+AS
+BEGIN
+	DECLARE @Sq1 NVARCHAR(1000) 
+	SET @Sql = 'Select * from '+@TableName 
+	EXEC (@Sql)
+END
+EXEC dbo._GetAll 'Category'
---
+++ 2212404_PTUDDT/lab02_2212404_PTUDDT/Lab2_Vidu2/Lab2_Vidu2/QuanLyGiaoVien.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_Vidu2
+{
+    internal class QuanLyGiaoVien
+    { 
+        public QuanLyGiaoVien()
+        {
+            dsGiaoVien = new GiaoVien();
+        }
+        public QuanLyGiaoVien()
+        {
+        }
+        public override string ToString()
+        {
+     
+        }
+}
